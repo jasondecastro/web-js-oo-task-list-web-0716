c5ccb152dbee5fe08c743a701641622cadd419e6
Jason Decastro <jasonrdecastro@gmail.com>
2016-09-20 20:55:00 -0400

    completed oo task lister lab

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,3 +1,40 @@
 'use strict';
 // Lists Controller
 
+class ListsController {
+  constructor() {
+    this.$addListForm = $('#add_list');
+    this.$listTitleInput = $('#list_title');
+    this.$selectListMenu = $('#select_list');
+    this.$addTaskForm = $('#add_task');
+    this.$wrapper = $('#wrapper');
+  }
+
+  addListFormListener() {
+    var self = this
+    this.$addListForm.submit(function(event) {
+      event.preventDefault()
+      var listTitle = self.$listTitleInput.val()
+      var list = new List(listTitle)
+      list.build()
+      self.$addTaskForm.one().show()
+      self.$listTitleInput.val('')
+    });
+  }
+
+  deleteListFormListener() {
+    self = this
+    this.$wrapper.on('click', '.destroy-list', function(){ 
+      var listId = parseInt($(this).parents('h2').next('ul').data('id'));
+      List.all.splice(listId, 1, null);
+      self.$selectListMenu.find('option[value="'+listId+'"]').remove();
+      $(this).parents('.list').remove();
+    });
+  }
+
+  init() {
+    this.$addTaskForm.hide()
+    this.addListFormListener()
+    this.deleteListFormListener()
+  }
+}
--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,28 @@
 'use strict';
 // List Model
 
+class List {
+  constructor(title) {
+    this.title = title
+    this.tasks = []
+    List.all.push(this)
+    this.id = List.all.length - 1
+  }
+
+  listEl() {
+    return `<div class="list"><h2><button class="destroy-list">x</button> ${this.title}</h2><ul id="list-${this.id}" data-id="${this.id}"></ul></div>`
+  }
+
+  optionEl() {
+    return `<option value="${this.id}">${this.title}</option>`
+  }
+
+  build() {
+    $(`#lists`).append(this.listEl())
+    $(`#select_list`).append(this.optionEl())
+
+  }
+}
+
+List.all = []
+
